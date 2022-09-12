243f8679b2cc0a5889f840a568b86a8610dd8099
Tamzid Israk <tamzidisrak12@gmail.com>
2022-09-12 21:31:02 +0600

    Login and Registeration splash login and registration design , nav graph add,

--- app/src/main/java/com/example/login_mvvm/View/MainActivity.java
+++ app/src/main/java/com/example/login_mvvm/View/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.login_mvvm.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.login_mvvm.R;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController= Navigation.findNavController(this,R.id.nav_host_fragment);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/example/login_mvvm/View/Splash.java
+++ app/src/main/java/com/example/login_mvvm/View/Splash.java
@@ -2,6 +2,9 @@ package com.example.login_mvvm.View;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +12,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.login_mvvm.R;
+import com.example.login_mvvm.databinding.FragmentSplashBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Splash#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Splash extends Fragment {
+    FragmentSplashBinding fragmentSplashBinding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Splash() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Splash.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Splash newInstance(String param1, String param2) {
-        Splash fragment = new Splash();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+        fragmentSplashBinding=FragmentSplashBinding.inflate(inflater,container,false);
+        return fragmentSplashBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
     }
 }
