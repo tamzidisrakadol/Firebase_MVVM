4283b2a9656d2815b28ba89a8ff20cf7587d2795
Tamzid Israk <tamzidisrak12@gmail.com>
2022-09-12 12:15:21 +0600

    Login and Registeration

---
+++ app/src/main/java/com/example/login_mvvm/View/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.login_mvvm.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login_mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
